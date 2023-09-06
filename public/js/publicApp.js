69bf2c74769cf97373ccfa9a7e05499a758f7054
kevit-dharmik-rathod <dharmik.rathod@kevit.io>
2023-09-06 12:01:02 +0530

    "Added try catch block"

--- public/js/publicApp.js
+++ public/js/publicApp.js
@@ -7,19 +7,23 @@ weatherForm.addEventListener("submit", (e) => {
   messageOne.textContent = "Loading...";
   messageTwo.textContent = "";
   const location = search.value;
-  async function display() {
-    await fetch(
-      "https://weatherapp-xh2h.onrender.com/weather/?address=" + location
-    ).then((res) => {
-      res.json().then((data) => {
-        if (data.error) {
-          messageOne.textContent = data.error;
-        } else {
-          messageOne.textContent = data.place;
-          messageTwo.textContent = data.forecast;
-        }
+  try {
+    async function display() {
+      await fetch(
+        "https://weatherapp-xh2h.onrender.com/weather/?address=" + location
+      ).then((res) => {
+        res.json().then((data) => {
+          if (data.error) {
+            messageOne.textContent = data.error;
+          } else {
+            messageOne.textContent = data.place;
+            messageTwo.textContent = data.forecast;
+          }
+        });
       });
-    });
+    }
+  } catch (error) {
+    messageOne.textContent = "Your internet Connection is broken";
   }
   display();
 });
